3f986a6def776e364137e4e631c70f600f31c8c6
AlonsoLo <alonso.lobato.jimenez@gmail.com>
2021-10-28 18:59:55 -0400

    update

--- roll_dice_with_comments.rb
+++ roll_dice_with_comments.rb
@@ -3,23 +3,24 @@
 # and a response being handled by the server with some ruby and html code.
 
 # This application rolls a number of dices with different number of sides
+# the information is passed to the server using the URL with parameters (query parameters)
 
 #==================================================================#
 
 # required library (default by Ruby) that includes a bunch of classes we can use to interact with servers and other networking tasks
 require 'socket'
 
-# helper method created to parse the URL into different parts we need to interact with the server
+# helper method created to parse the HTTP request line ("GET /?rolls=2&sides=6 HTTP/1.1") into different parts we need to interact with at the server
 def parse_request(request_line)
-  http_method, path_and_params, http = request_line.split(' ')
-  path, params = path_and_params.split('?')
+  http_method, path_and_params, http = request_line.split(' ') # uses the spaces in the HTTP request line to divide into three: method, path, scheme
+  path, params = path_and_params.split('?') # uses the '?' in the path to separate the path and params
 
-  params = params.split('&').each_with_object({}) do |pair, hash|
+  params = params.split('&').each_with_object({}) do |pair, hash| # uses the '&' to create a hash where keys are the param names and vals are the params values
     key, value = pair.split('=')
     hash[key] = value
   end
 
-  [http_method, path, params] # return values from the method is an array with the three main parts of the URL (request)
+  [http_method, path, params] # return values from the method is an array with the three main parts of the HTTP request (this is a pretty standard structure)
 end
 
 server = TCPServer.new('localhost', 3003) # instantiating an object of TCP server class, passing two arguments, the server name and the port
@@ -27,15 +28,16 @@ loop do # main Ruby code in the application
   client = server.accept # openning a connection and storing it in a client object we'll use to interact with the system
 
   request_line = client.gets # 'gets' get the first line of the request: a http method, a path and some parameters
-  next if !request_line || request_line =~ /favicon/ # This will terminate the loop early if the request generates a problem, allowing us to know that the rest of our code will not need to handle these situations.
+  next if !request_line || request_line =~ /favicon/ # This will terminate the loop early if the request generates a problem, allowing us to know that the rest of our code will not need to handle these situations. Kind of a guard clause
 
   puts request_line # this line just prints the request_line into the consol which the code client.puts request_line sends back to the client to be displayed in the browser
-  # client.puts request_line # this displays the request line to the browsers
+  # client.puts request_line # this displays the request line to the browsers (commented out for now)
 
-  http_method, path, params = parse_request(request_line) # Executing the helper method to parse the URL
+  http_method, path, params = parse_request(request_line) # Executing the helper method to parse the HTTP request, stores each element in array into separate variables
 
   # client.puts "HTTP/1.1 200 OK\r\n\r\n" # Before we completed the application with required status code and other fields, we needed this line to avoid problems with browsers, like Chrome, that required well-formed response
 
+  # This section handles the response. Note that there is html embeded
   # http response status code (mandatory)
   client.puts 'HTTP/1.1. 200 OK'
 
@@ -54,6 +56,7 @@ loop do # main Ruby code in the application
 
   client.puts '<h1>Rolls</h1>'
 
+  # This section executes the business logic
   rolls = params['rolls'].to_i # everything in an http cycle is text, so sometimes we have to convert variables to integers to operate with them
   sides = params['sides'].to_i
 
@@ -64,5 +67,6 @@ loop do # main Ruby code in the application
 
   client.puts '</body>'
   client.puts '</html>'
+  
   client.close
 end
--- server.rb
+++ server.rb
@@ -1,13 +1,20 @@
+# require the socket Ruby library
 require "socket"
 
-server = TCPServer.new("localhost", 3003)
-loop do
-  client = server.accept
+# create a TCP Server for connection
+# we'll build our server on top of it
+# note how it takes hostname and port as arguments, which specifies where the server
+# would accept requests.
 
-  request_line = client.gets
-  puts request_line
+server = TCPServer.new("localhost", 3003)
+# the loop executes the application code in loops until the program exits or the process running inside stops
+loop do
+  client = server.accept # '.accept' is an TCPServer method that accepts an incomming connection and returns a TCPSocket obj; we store it in a local var
+
+  request_line = client.gets # '.gets' is a Kernel method that captures the first line of whatever the HTTP request is (method + path + scheme -> GET / HTTP/1.1)
+  puts request_line # this line prints the captured first line to the terminal
 
   client.puts "HTTP/1.1 200 OK\r\n\r\n"
-  client.puts request_line
+  client.puts request_line # and finally this line sends the same string to the user for display
   client.close
 end

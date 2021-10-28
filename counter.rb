0b5f57f065da550e2804d9697d7c66273be8de60
AlonsoLo <alonso.lobato.jimenez@gmail.com>
2021-10-28 16:49:29 -0400

    first commit

---
+++ counter.rb
@@ -0,0 +1,55 @@
+# This application artifitially persist the state in an URL
+# Remember that HTTP is stateless, which means that each request is handled separately by the server.
+
+# We basically maintain the code from the roll_dice program
+# to make the program work even when no parameters are passed we update the helper method so split is called either on a parameter or an empty string if no param is passed (line 12)
+require 'socket'
+
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split(' ')
+  path, params = path_and_params.split('?')
+
+  params = (params || '').split('&').each_with_object({}) do |pair, hash|
+    key, value = pair.split('=')
+    hash[key] = value
+  end
+
+  [http_method, path, params]
+end
+
+server = TCPServer.new('localhost', 3003)
+loop do
+  client = server.accept
+  request_line = client.gets
+  next if !request_line || request_line =~ /favicon/
+
+  puts request_line
+
+  http_method, path, params = parse_request(request_line)
+
+  client.puts 'HTTP/1.1. 200 OK'
+  client.puts 'Content-Type: text/html'
+  client.puts
+  client.puts '<html>'
+  client.puts '<body>'
+  client.puts '<pre>'
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts '</pre>'
+
+  client.puts '<h1>Counter</h1>'
+
+  number = params['number'].to_i
+  client.puts "<p>The current number is #{number}.</p>"
+
+  # this code adds two links that update the number parameters
+  # notice the use of href to link to a new page where the 'number' parameter gets increased or decreased from current value
+  # if no parameter is passed, number will equal 0 as empty string to_i in ruby returns 0
+  client.puts "<a href='?number=#{number + 1}'>Increase by one</a>"
+  client.puts "<a href='?number=#{number - 1}'>Decrease by one</a>"
+
+  client.puts '</body>'
+  client.puts '</html>'
+  client.close
+end
---
+++ roll_dice_with_comments.rb
@@ -0,0 +1,68 @@
+# This programs manually handles a text-based request-response cycle
+# with the request coming from user via URL entered into a browser (including some query strings)
+# and a response being handled by the server with some ruby and html code.
+
+# This application rolls a number of dices with different number of sides
+
+#==================================================================#
+
+# required library (default by Ruby) that includes a bunch of classes we can use to interact with servers and other networking tasks
+require 'socket'
+
+# helper method created to parse the URL into different parts we need to interact with the server
+def parse_request(request_line)
+  http_method, path_and_params, http = request_line.split(' ')
+  path, params = path_and_params.split('?')
+
+  params = params.split('&').each_with_object({}) do |pair, hash|
+    key, value = pair.split('=')
+    hash[key] = value
+  end
+
+  [http_method, path, params] # return values from the method is an array with the three main parts of the URL (request)
+end
+
+server = TCPServer.new('localhost', 3003) # instantiating an object of TCP server class, passing two arguments, the server name and the port
+loop do # main Ruby code in the application
+  client = server.accept # openning a connection and storing it in a client object we'll use to interact with the system
+
+  request_line = client.gets # 'gets' get the first line of the request: a http method, a path and some parameters
+  next if !request_line || request_line =~ /favicon/ # This will terminate the loop early if the request generates a problem, allowing us to know that the rest of our code will not need to handle these situations.
+
+  puts request_line # this line just prints the request_line into the consol which the code client.puts request_line sends back to the client to be displayed in the browser
+  # client.puts request_line # this displays the request line to the browsers
+
+  http_method, path, params = parse_request(request_line) # Executing the helper method to parse the URL
+
+  # client.puts "HTTP/1.1 200 OK\r\n\r\n" # Before we completed the application with required status code and other fields, we needed this line to avoid problems with browsers, like Chrome, that required well-formed response
+
+  # http response status code (mandatory)
+  client.puts 'HTTP/1.1. 200 OK'
+
+  # http response header(optional)
+  client.puts 'Content-Type: text/html' # The "Content-Type" header specifies how the response body is interpretated and displayed
+  client.puts # An empty line is requires between the header and the body
+
+  # http response body: html code
+  client.puts '<html>'
+  client.puts '<body>'
+  client.puts '<pre>' # the following lines are used for debugging, to see the value of those variables printed in screen. We'll keep them inside this <pre> tag as diagnosis information. <pre> preserves the text in the way is preformated in the html file
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts '</pre>'
+
+  client.puts '<h1>Rolls</h1>'
+
+  rolls = params['rolls'].to_i # everything in an http cycle is text, so sometimes we have to convert variables to integers to operate with them
+  sides = params['sides'].to_i
+
+  rolls.times do
+    roll = rand(sides) + 1
+    client.puts '<p>', roll, '</p>'
+  end
+
+  client.puts '</body>'
+  client.puts '</html>'
+  client.close
+end
